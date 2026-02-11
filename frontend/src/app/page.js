06b3f65f144913d861f7fdeb359f76757bc7a678
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2026-02-12 00:17:16 +0530

    modified page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,8 +1,47 @@
+"use client";
+import { useState } from "react";
+import { Navbar } from "../components/ui/Navbar";
+import { Sidebar } from "../components/ui/Sidebar";
+import ChatPanel from "../components/ChatPanel";
+import CodeEditor from "../components/CodeEditor";
+import PreviewPanel from "../components/PreviewPanel";
 
-export default function Home() {
+export default function Page() {
+  const [code, setCode] = useState("");
+  const [versions, setVersions] = useState([]);
+
+  async function handleGenerate(userPrompt) {
+    const res = await fetch("http://localhost:5000/generate", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        userPrompt,
+        previousCode: code
+      })
+    });
+
+    const data = await res.json();
+
+    // Save version
+    setVersions([...versions, data.code]);
+
+    // Update code
+    setCode(data.code);
+  }
+
   return (
-    <div>
-      Home
+    <div className="app-container">
+      <Navbar />
+
+      <div className="main-layout">
+        <Sidebar />
+
+        <div className="center-layout">
+          <ChatPanel onSend={handleGenerate} />
+          <CodeEditor code={code} setCode={setCode} />
+          <PreviewPanel code={code} />
+        </div>
+      </div>
     </div>
   );
 }
